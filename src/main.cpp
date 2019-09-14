af389cb1ef94365977ede0d610c6e4544e8303eb
Bruno Andreotti <bandreotti@protonmail.com>
2019-09-14 14:38:36 -0300

    Ler arquivo de entrada e inicializar equipe

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,45 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 #include "equipe.h"
 
 int main(int argc, char* argv[]) {
-    Equipe eq = Equipe(10);
-    
-    eq.inserir_aresta(5, 6);
-    eq.inserir_aresta(2, 3);
-    eq.inserir_aresta(3, 4);
-    eq.inserir_aresta(4, 2); // Não estamos checando ciclos
-    eq.inserir_aresta(8, 8); // ...laços
-    eq.inserir_aresta(5, 6); // ...ou arestas paralelas.
 
-    eq.imprimir_arestas();
+    if (argc != 2) {
+        std::cerr << "Número errado de argumentos da linha de comando!" << std::endl;
+        return 1;
+    }
+
+    auto arquivo_entrada = std::ifstream(std::string(argv[1]));
+    std::string linha;
+    std::getline(arquivo_entrada, linha);
+    auto sstream_linha = std::stringstream(linha);
+
+    int num_alunos, num_arestas, num_instrucoes;
+    sstream_linha >> num_alunos >> num_arestas >> num_instrucoes;
+
+    Equipe equipe = Equipe(num_alunos);
+
+    std::getline(arquivo_entrada, linha);
+    sstream_linha = std::stringstream(linha);
+
+    for (int i = 0; i < num_alunos; i++) {
+        int idade;
+        // Por enquanto, estamos ignorando a idade lida
+        // @TODO: Armazenar as idades dos alunos.
+        sstream_linha >> idade;
+    }
+
+    for (int i = 0; i < num_arestas; i++) {
+        std::getline(arquivo_entrada, linha);
+        sstream_linha = std::stringstream(linha);
+        int x, y;
+        sstream_linha >> x >> y;
+        equipe.inserir_aresta(x, y);
+    }
+
+    equipe.imprimir_arestas();
+
     return 0;
 }
